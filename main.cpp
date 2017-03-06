4f7a14daed12b9f33aa37b043affdd5fccf2c1cd
Felix Kugler <e1526144@student.tuwien.ac.at>
2017-03-06 20:25:58 +0100

    draw window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,67 @@
+#define GLEW_STATIC
+
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-using namespace std;
+int width = 1280, height = 720;
+const char *title = "Guardian's Awakening: The Mending of the Sky";
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+GLFWwindow *initGLFW() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    //glfwWindowHint(GLFW_SAMPLES, 4);
+
+    GLFWwindow *window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return window;
+    }
+
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    return window;
 }
 
+bool initGLEW() {
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main(void) {
+    GLFWwindow* window = initGLFW();
+    if (window == NULL) {
+        return 0;
+    }
+
+    // Set OpenGL options
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_MULTISAMPLE);
+
+    if (!initGLEW()) {
+        return 0;
+    }
+
+    glViewport(0, 0, width, height);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
