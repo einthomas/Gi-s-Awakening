2f50916bce810822dba824aef6e9e7232188f4ec
Felix Kugler <relgukxilef@gmail.com>
2017-03-07 23:46:58 +0100

    fix reading of shaders

--- Shader.h
+++ Shader.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #define GLEW_STATIC
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
-#include <GLM\glm.hpp>
-#include <glm\gtc\matrix_transform.hpp>
-#include <GLM\gtc\type_ptr.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <string>
 #include <fstream>
 
