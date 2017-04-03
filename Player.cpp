ce082e4eac5ef6a1901ac63f29948e96c31c3111
Felix Kugler <relgukxilef@gmail.com>
2017-04-03 22:51:06 +0200

    put player back to start when they fall out of the map

--- Player.cpp
+++ Player.cpp
@@ -7,7 +7,7 @@ Player::Player(glm::vec3 position, glm::vec3 size) {
 
 void Player::update(float delta, float gravity, const Level &level) {
     // update projectiles
-    for (int i = 0; i < projectiles.size(); i++) {
+    for (unsigned short i = 0; i < projectiles.size(); i++) {
         if (glm::length(projectiles[i].object3D.position - position) > Projectile::DESPAWN_DISTANCE) {
             projectiles.erase(projectiles.begin() + i);
         } else {
@@ -30,6 +30,11 @@ void Player::update(float delta, float gravity, const Level &level) {
         // frame-rate intependent position calculation
         position.z += velocityZ * delta - 0.5 * gravity * delta * delta;
         velocityZ -= gravity * delta;
+    }
+
+    // check whether player fell out of map
+    if (position.z < -10) { // TODO: unhardcode bottom of map
+        position = level.start + glm::vec3(0.f, 0.f, 1.f);
     }
 
     // check for and handle player intersection
