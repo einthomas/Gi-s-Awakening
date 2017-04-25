4595e4400a3de78095c6ab85f272b1d5c272d0f6
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-04-25 23:41:25 +0200

    Implement perlin noise flow fields

---
+++ MathUtil.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class MathUtil {
+public:
+    static glm::vec3 limit(glm::vec3 vector, float magnitude);
+};
--- ParticleSystem.h
+++ ParticleSystem.h
@@ -2,29 +2,40 @@
 
 #include <GL/glew.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
 #include <vector>
 
 #include "Shader.h"
+#include "MathUtil.h"
 
 class ParticleSystem {
 public:
     static void init();
-    static void makeParticle(const glm::vec3 &position, const glm::vec3 &speed, const glm::vec3 &normal, const glm::vec4 &color);
+    static void beginParticleGroup(const glm::vec3 &planeNormal);
+    static void makeParticle(const glm::vec3 &position); // , const glm::vec3 &speed, const glm::vec3 &planeNormalVector, const glm::vec3 &normal, const glm::vec4 &color, const glm::vec3 &perlinOffset);
     static void draw(float delta, const glm::mat4 &viewMatrix, const glm::mat4 &projectionMatrix);
     static void update(float delta);
 
 private:
+    struct ParticleGroup {
+        glm::vec3 planeNormal;
+        int perlinFlowField;
+    };
+
     struct Particle {
-        glm::vec3 originPosition, position, speed;
-        glm::vec3 normal;
-        float amplitude, frequency;
+        glm::vec3 originPosition, position;
         glm::vec4 color;
+        int perlinFlowField;
+        glm::vec3 velocity;
     };
 
     static const int MAX_PARTICLES = 1000;
+    static const int PERLIN_FLOW_FIELD_SIZE = 20;
+    static const int PERLIN_FLOW_FIELDS = 3;
     static int particleCount;
     static int bufferStart;
     static int bufferEnd;
+    static int currentPerlinFlowField;
     static Shader shader;
     static GLuint VAO;
     static GLuint VBO;
@@ -33,4 +44,6 @@ private:
     static std::vector<Particle> particles;
     static GLfloat particlePositions[];
     static GLfloat particleColors[];
+    static std::vector<std::vector<std::vector<glm::vec3>>> perlinFlowFields;
+    static std::vector<std::vector<std::vector<float>>> perlinAngles;
 };
